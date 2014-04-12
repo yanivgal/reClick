8ddfcb0b9f8d12cebdd4f782ff9dde88a6f1219e
Yaniv Gal <yanivgal123@gmail.com>
2014-04-12 14:33:48 +0300

    Started Login screen implementation

--- src/com/reclick/framework/App.java
+++ src/com/reclick/framework/App.java
@@ -1,10 +1,16 @@
 package com.reclick.framework;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
 
 public class App {
+	
+	private final static String TAG = "App";
 	
 	public static int version(Context context) {
 	    try {
@@ -15,5 +21,30 @@ public class App {
 	        // should never happen
 	        throw new RuntimeException("Could not get package name: " + e);
 	    }
+	}
+	
+	public static final String md5(final String s) {
+	    final String MD5 = "MD5";
+	    try {
+	        // Create MD5 Hash
+	        MessageDigest digest = java.security.MessageDigest
+	                .getInstance(MD5);
+	        digest.update(s.getBytes());
+	        byte messageDigest[] = digest.digest();
+
+	        // Create Hex String
+	        StringBuilder hexString = new StringBuilder();
+	        for (byte aMessageDigest : messageDigest) {
+	            String h = Integer.toHexString(0xFF & aMessageDigest);
+	            while (h.length() < 2)
+	                h = "0" + h;
+	            hexString.append(h);
+	        }
+	        return hexString.toString();
+
+	    } catch (NoSuchAlgorithmException e) {
+	        Log.e(TAG, e.getMessage());
+	    }
+	    return "";
 	}
 }
