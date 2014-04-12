a0fca07ec87b62fd2c17e47bed5d39d5b42c0ae8
Yaniv Gal <yanivgal123@gmail.com>
2014-04-12 15:47:17 +0300

    Started Splash screen implementation

--- src/com/reclick/reclick/MainActivity.java
+++ src/com/reclick/reclick/MainActivity.java
@@ -1,14 +1,7 @@
 package com.reclick.reclick;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.reclick.framework.Prefs;
-import com.reclick.gcm.Gcm;
-
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -19,10 +12,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		if (checkPlayServices()) {
-			new Gcm(this).register();
-		}
 	}
 
 	@Override
@@ -31,36 +20,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	public void afterGcm() {
-		String regId = Prefs.getGcmRegId(this);
-		
-		if (regId.isEmpty()) {
-			Log.e(TAG, "Error creating GCM registration ID");
-			finish();
-		}
-		
-		Log.e(TAG, "GCM registration ID - " + regId);
-	}
-	
-	/**
-	 * Check the device to make sure it has the Google Play Services APK. If
-	 * it doesn't, display a dialog that allows users to download the APK from
-	 * the Google Play Store or enable it in the device's system settings.
-	 */
-	private boolean checkPlayServices() {
-	    int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-	    if (resultCode != ConnectionResult.SUCCESS) {
-	        if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-	            GooglePlayServicesUtil.getErrorDialog(resultCode, this,
-	                    9000).show();
-	        } else {
-	            Log.i(TAG, "This device is not supported.");
-	            finish();
-	        }
-	        return false;
-	    }
-	    return true;
-	}
-
 }
